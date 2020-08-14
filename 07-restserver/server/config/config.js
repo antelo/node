27fff0c49294f07f21269c1c0cdec84c8ea2a5f0
antelo81 <antelo03@msn.com>
2020-08-14 10:21:10 +0200

    Variable de entorno mongo

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:sDwhR9CvM2NpZEht@cluster0.idw7h.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
