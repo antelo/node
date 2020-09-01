ce8785523d56ef9895a5fb5e0101e0112cef1923
antelo81 <antelo03@msn.com>
2020-09-01 13:38:02 +0200

    Google Sign-In

--- 07-restserver/server/routes/login.js
+++ 07-restserver/server/routes/login.js
@@ -69,15 +69,84 @@ async function verify(token) {
     const payload = ticket.getPayload();
 
     console.log(payload);
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    };
+
 }
 
 
-app.post('/google', (req, res) => {
+app.post('/google', async (req, res) => {
 
     let token = req.body.idtoken;
-    verify(token);
-    res.json({
-        token: token
+
+    let googleUser = await verify(token)
+                        .catch(e => {
+                            return res.status(403).json({
+                                ok: false,
+                                err: e
+                            });
+                        });
+    Usuario.findOne({ email: googleUser.email}, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (usuarioDB) {
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Debe usar su autenticación normal'
+                    }
+                });
+            } else {
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN});
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                });
+            }
+        } else {
+            // Si el usuario no existe en nuestra bbdd
+            let usuario = new Usuario();
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+
+            usuario.save( (err, usuarioDB) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    });
+                }
+
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN});
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                });
+
+            });
+
+        }
+
     });
 });
 
