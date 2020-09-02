b0522bb29684728cf5f9f3ee72eb4e79397b733f
antelo81 <antelo03@msn.com>
2020-09-02 12:06:42 +0200

    Subida de archivos

--- 07-restserver/server/middlewares/authentication.js
+++ 07-restserver/server/middlewares/authentication.js
@@ -42,4 +42,24 @@ let verificaAdminRole = (req, res, next) => {
 
 };
 
-module.exports = { verificaToken, verificaAdminRole };
+// ================
+// Verificar token para imagen
+// ================
+
+let verificaTokenImg = (req, res, next) => {
+
+    let token = req.query.token;
+
+    jwt.verify( token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+    });
+};
+
+module.exports = { verificaToken, verificaAdminRole, verificaTokenImg };
